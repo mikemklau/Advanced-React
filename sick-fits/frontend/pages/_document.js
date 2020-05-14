81412ae61e9314a56179715964d9efdb8204e213
Michael Lau <michael@umidigital.co.uk>
2020-05-14 13:55:48 +0100

    initial push - episode 10

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+// https://github.com/zeit/next.js/blob/master/examples/with-styled-components/pages/_document.js
